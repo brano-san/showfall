988626d9729fd889560e38ea6caf6f4c68501976
brano-san <branoverv10@bk.ru>
2024-09-29 12:43:34 +0300

    Add showfall

---
+++ forms/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace showfall
+{
+    public partial class Main : Form
+    {
+        private const int SnowflakeCount = 50;
+
+        private Timer timer;
+        private Snowflake[] snowflakes;
+        private Image snowflakeImage;
+
+        public Main()
+        {
+            InitializeComponent();
+            InitializeSnowfall();
+        }
+
+        private void InitializeSnowfall()
+        {
+            snowflakeImage = Properties.Resources.show;
+
+            snowflakes = new Snowflake[SnowflakeCount];
+
+            for (var i = 0; i < SnowflakeCount; i++)
+            {
+                snowflakes[i] = new Snowflake(Width, Height);
+            }
+
+            timer = new Timer();
+            timer.Interval = 5;
+            timer.Tick += Timer_Tick;
+            timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            foreach (var snowflake in snowflakes)
+            {
+                snowflake.Fall();
+                Invalidate(snowflake.GetBounds());
+                Invalidate(snowflake.GetPrevBounds());
+            }
+
+        }
+
+        private void Main_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            foreach (var snowflake in snowflakes)
+            {
+                g.DrawImage(snowflakeImage, snowflake.X, snowflake.Y, 20, 20);
+            }
+        }
+    }
+}
---
+++ src/Snowflake.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+
+namespace showfall
+{
+    internal class Snowflake
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int PrevX { get; private set; }
+        public int PrevY { get; private set; }
+        public int Speed { get; set; }
+        private int formWidth;
+        private int formHeight;
+        private static Random random = new Random();
+
+        public const int ShowWidth = 20, ShowHeigth = 20;
+
+        public Snowflake(int formWidth, int formHeight)
+        {
+            this.formWidth = formWidth;
+            this.formHeight = formHeight;
+
+            X = random.Next(0, formWidth);
+            Y = random.Next(-formHeight, 0);
+            Speed = random.Next(2, 10);
+        }
+
+        public void Fall()
+        {
+            PrevX = X;
+            PrevY = Y;
+
+            Y += Speed;
+
+            if (Y > formHeight)
+            {
+                Y = -10;
+                X = random.Next(0, formWidth);
+                Speed = random.Next(2, 10);
+            }
+        }
+
+        public Rectangle GetBounds()
+        {
+            return new Rectangle(X, Y, ShowWidth, ShowHeigth);
+        }
+
+        public Rectangle GetPrevBounds()
+        {
+            return new Rectangle(PrevX, PrevY, ShowWidth, ShowHeigth);
+        }
+    }
+}
